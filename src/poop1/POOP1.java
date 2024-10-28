4006431a3e069d2ad25637f37b07881999953d0b
Rickgar1105 <ricardo115@comunidad.unam.mx>
2024-10-27 20:20:16 -0600

    Practica 1

---
+++ src/poop1/POOP1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop1;
+
+/**
+ * Clase principal del proyecto POOP1
+ * @AUTOR Ricardo Emmanuel Galicia Tequianes
+ */
+public class POOP1 {
+
+    /**
+     * Método principal que ejecuta la aplicación
+     * @param args los argumentos de la línea de comandos
+     */
+    public static void main(String[] args) {
+        // Código de la aplicación
+        System.out.println("¡Hola mundo!");
+    }
+}
+
+    
+
+
